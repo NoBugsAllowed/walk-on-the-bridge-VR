de6c6ab9fde67f1e44166839fa2020894f2352f7
Patryk Szczerbiński <szczerbinskip@student.mini.pw.edu.pl>
2019-12-05 22:19:00 +0100

    bridge rotation, fixed error in reading data from mobile app

---
+++ VRModule/Assets/Scripts/BridgeRotation.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BridgeRotation : MonoBehaviour
+{
+    private const float MAX_ANGLE = 10.0f;
+    private const float ROTATION_POINT_Y_OFFSET = 8.0f;
+    private const float ROTATION_POINT_Z_OFFSET = 4.0f;
+    private float angle = 0;
+    private Vector3 rotateAroundPoint;
+    private Vector3 initialPosition;
+    private Quaternion initialRotation;
+
+    public double Offset { get; set; }
+
+    void Start()
+    {
+        initialPosition = transform.position;
+        initialRotation = transform.rotation;
+        rotateAroundPoint = new Vector3(transform.position.x, transform.position.y + ROTATION_POINT_Y_OFFSET, transform.position.z - ROTATION_POINT_Z_OFFSET);
+    }
+
+    void Update()
+    {
+        // Update bridge rotation
+        transform.position = initialPosition;
+        transform.rotation = initialRotation;
+        transform.RotateAround(rotateAroundPoint, Vector3.right, OffsetToAngle(Offset));
+    }
+
+    private float OffsetToAngle(double offset)
+    {
+        return (float)(Offset * MAX_ANGLE);
+    }
+}
---
+++ VRModule/Assets/Scripts/Init.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using UnityEngine;
+
+public class Init : MonoBehaviour
+{
+    private const string MOBILE_APP_KEY = "WALK-ON-THE-BRIDGE-MOBILE";
+    private const string VR_MODULE_KEY = "WALK-ON-THE-BRIDGE-VR";
+    private const int PORT = 9009;
+    private BridgeRotation bridgeScript;
+    private IPEndPoint broadcastAddr;
+    private struct UdpState
+    {
+        public UdpClient client;
+        public IPEndPoint endPoint;
+        public UdpState(UdpClient c, IPEndPoint e)
+        {
+            client = c;
+            endPoint = e;
+        }
+    }
+
+    void Start()
+    {
+        //Get Bridge object
+        GameObject bridge = GameObject.Find("StaticBridge");
+        bridgeScript = bridge.GetComponent<BridgeRotation>();
+
+        // Start TCP listener
+        IPAddress localAddr = IPAddress.Any;
+        TcpListener listener = new TcpListener(localAddr, PORT);
+        TcpClient mobileApp = null;
+        listener.Start();
+        Debug.Log("TCP Listener started");
+        listener.BeginAcceptTcpClient(new AsyncCallback(AcceptMobileAppCallback), listener);
+
+        // 
+        broadcastAddr = new IPEndPoint(IPAddress.Any, PORT);
+        UdpClient client = new UdpClient();
+        UdpState s = new UdpState(client, broadcastAddr);
+        client.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+        client.Client.Bind(broadcastAddr);
+        Debug.Log("Waiting for mobile app");
+        client.BeginReceive(UdpMessageReceiveCallback, s);
+    }
+
+    private void UdpMessageReceiveCallback(IAsyncResult ar)
+    {
+        bool mobileAppFound = false;
+
+        UdpState us = (UdpState)(ar.AsyncState);
+        UdpClient u = us.client;
+        IPEndPoint e = us.endPoint;
+
+        byte[] receivedBytes = u.EndReceive(ar, ref e);
+        string receivedString = Encoding.ASCII.GetString(receivedBytes);
+        Debug.Log("Received message: " + receivedString);
+
+        // If it is our mobile app
+        if (receivedString.Equals(MOBILE_APP_KEY))
+        {
+            try
+            {
+                Debug.Log("Mobile app found: " + e.Address.ToString() + ":" + e.Port.ToString());
+
+                // Send response with proper key
+                byte[] sendBytes = Encoding.ASCII.GetBytes(VR_MODULE_KEY);
+                u.Send(sendBytes, sendBytes.Length, e);
+
+                Debug.Log("Responded with a message: " + VR_MODULE_KEY);
+                mobileAppFound = true;
+            }
+            catch (SocketException ex)
+            {
+                Debug.Log("Sending response to mobile app failed: " + ex.Message);
+            }
+        }
+        // Register handler again to wait for mobile app again
+        if (!mobileAppFound)
+        {
+            UdpClient client = new UdpClient();
+            UdpState s = new UdpState(client, broadcastAddr);
+            client.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+            client.Client.Bind(broadcastAddr);
+            Debug.Log("Waiting for mobile app");
+            client.BeginReceive(UdpMessageReceiveCallback, s);
+        }
+    }
+
+    private void AcceptMobileAppCallback(IAsyncResult ar)
+    {
+        TcpListener listener = (TcpListener)ar.AsyncState;
+        TcpClient mobileApp = listener.EndAcceptTcpClient(ar);
+
+        byte[] bytes = new byte[sizeof(double)];
+        using (NetworkStream stream = mobileApp.GetStream())
+        {
+            int length;
+            // Read incomming message				
+            while ((length = stream.Read(bytes, 0, bytes.Length)) != 0)
+            {
+                Array.Reverse(bytes, 0, bytes.Length);
+                double offset = BitConverter.ToDouble(bytes, 0);
+                bridgeScript.Offset = offset;
+                Debug.Log("Server message received as: " + offset.ToString());
+            }
+        }
+    }
+}
