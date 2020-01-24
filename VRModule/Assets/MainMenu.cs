b5b9a6a3210be46a2e8dde16233d8ecd400ff7ac
Patryk Szczerbiński <szczerbinskip@student.mini.pw.edu.pl>
2020-01-24 02:34:05 +0100

    Menu added

---
+++ VRModule/Assets/MainMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    public void RunMain()
+    {
+        SceneManager.LoadScene("Main");
+    }
+    public void RunVolcano()
+    {
+        SceneManager.LoadScene("Volcano");
+    }
+    public void Exit()
+    {
+        Debug.Log("Exit");
+        Application.Quit();
+    }
+}
